5f829dd19d8f438e8a96f1c271dfba8c44c2fd25
subin <tnqls1685@gmail.com>
2025-12-02 10:58:51 +0900

    관리자페이지 반응형

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -2,85 +2,87 @@ import '../css/Review.css'
 import { FaStar } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import { HiArrowLeft } from "react-icons/hi";
+import { useState } from 'react';
 
-function Review(){
-const navigate = useNavigate();
-    return(
-    <div className="mmainpage">
-        <div className='reviewmain'>
-            <p
-            style={{ cursor: "pointer"}}
-                onClick={() => navigate("/")}
-            >
-                <HiArrowLeft />
-            </p>
-            <div className='reviewhead'>
-                별점 리뷰
-                <div className='reviewhead2'>
-                    <div>
-                        <FaStar color="gold" size={35} />
-                        4.3
+function Review() {
+    const navigate = useNavigate();
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const items = ["0.5", "1.0", "1.5", "2.0", "2.5", "3.0", "3.5", "4.0", "4.5", "5.0" ];
+    return (
+        <div className="mmainpage">
+            <div className='reviewmain'>
+                <p
+                    style={{ cursor: "pointer" }}
+                    onClick={() => navigate("/")}
+                >
+                    <HiArrowLeft />
+                </p>
+                
+                <div className='reviewhead'>
+                    별점 리뷰
+                    <div className='reviewhead2'>
+                        <div>
+                            <FaStar color="gold" size={35} />
+                            4.3
+                        </div>
+                        <p>전체 n개의 리뷰</p>
                     </div>
-                    <p>전체 n개의 리뷰</p>
                 </div>
-            </div>
-            <div className='review'>
+                <div className='review'>
 
-                <div className='reviewcard'>
-                    <div className='spoint'>
-                        <FaStar color="gold" size={35} />
-                            <p>0.5</p>
-                            <p>1.0</p>
-                            <p>1.5</p>
-                            <p>2.0</p>
-                            <p>2.5</p>
-                            <p>3.0</p>
-                            <p>3.5</p>
-                            <p>4.0</p>
+                    <div className='reviewcard'>
+                        <div className='spointbu'>
+                            <FaStar color="gold" size={35} />
+                            {items.map((item, index) => (
+                                <p
+                                    key={index}
+                                    className={`selectspoint ${activeIndex === index ? "active" : ""}`}
+                                    onClick={() => setActiveIndex(index)}
+                                >
+                                    {item}
+                                </p>
+                               ))}
+                               
+                        </div>
+
+                        <textarea className='reviewtxt'
+                            type='text'
+                            placeholder='리뷰내용을 작성해주세요.'
+                            name='Review'   
+                        />
+
+                        <button onClick={() => (alert("저장되었습니다."))}>저장</button>
+
+
+
+                    </div>
+
+                    <div className='reviewcard'>
+                        <div className='spoint'>
+                            <FaStar color="gold" size={35} />
                             <p>4.5</p>
-                            <p>5.0</p>
+                        </div>
+                        <div className='reviewcard2'>
+                            <p>이수빈</p>
+                            <p>2025-11-27</p>
+                            <p>내용.......</p>
+                        </div>
                     </div>
-                    
-                    <input className='reviewtxt' 
-                    type='text' 
-                    placeholder='리뷰내용을 작성해주세요' 
-                    name='Review'
-                    style={{ 
-                        textAlign: "start",
-                        paddingBottom: "130px",
-                        boxSizing: "border-box"
-                     }}
-                     />
-                        
-                        
-                    
-                </div>
-
-                <div className='reviewcard'>
-                    <div className='spoint'>
-                        <FaStar color="gold" size={35} />
-                            <p>4.5</p>
-                    </div>
-                    <div className='reviewcard2'>
-                        <p>이수빈</p>
-                        <p>2025-11-27</p>
-                        <p>내용.......</p>
-                    </div>
-                </div>
-                <div className='reviewcard'>
-                    <div className='spoint'>
-                        <FaStar color="gold" size={35} />
+                    <div className='reviewcard'>
+                        <div className='spoint'>
+                            <FaStar color="gold" size={35} />
                             <p>4.0</p>
-                    </div>
-                    <div className='reviewcard2'>
-                        <p>이수빈</p>
-                        <p>2025-11-27</p>
-                        <p>내용.......</p>
+                        </div>
+                        <div className='reviewcard2'>
+                            <p>이수빈</p>
+                            <p>2025-11-27</p>
+                            <p>내용.......</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 }
 
