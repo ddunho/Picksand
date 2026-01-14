f2f2f88cd1bf8bbdaef17e9184d51bf94f83cb2a
한승희 <dopicher@gmail.com>
2026-01-14 10:35:26 +0900

    지도 ui 개선

--- src/components/GGMap.js
+++ src/components/GGMap.js
@@ -77,6 +77,7 @@ export default function GGMap({handleGPStoggle, userInfo, shopInfos}) {
 
         mapRef.current.setCenter(newPos);
         setSelectedPlace(shopInfos[i]);
+        setCurrentSelectedShopAddr(i)
 
       });
       
